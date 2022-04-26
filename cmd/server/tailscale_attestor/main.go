9290db5009fe67d555cdd2aa1f949766e05d5acb
Johan Siebens <johan.siebens@gmail.com>
2022-04-26 07:57:29 +0200

    initial commit

---
+++ cmd/server/tailscale_attestor/main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"github.com/hashicorp/hcl"
+	"github.com/spiffe/go-spiffe/v2/spiffeid"
+	"github.com/spiffe/spire-plugin-sdk/pluginmain"
+	nodeattestorv1 "github.com/spiffe/spire-plugin-sdk/proto/spire/plugin/server/nodeattestor/v1"
+	configv1 "github.com/spiffe/spire-plugin-sdk/proto/spire/service/common/config/v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"tailscale.com/client/tailscale"
+	"tailscale.com/ipn/ipnstate"
+	"tailscale.com/types/key"
+)
+
+const (
+	PluginName = "ts"
+)
+
+type Config struct {
+	trustDomain spiffeid.TrustDomain
+}
+
+// Plugin implements the NodeAttestor plugin
+type Plugin struct {
+	nodeattestorv1.UnimplementedNodeAttestorServer
+	configv1.UnimplementedConfigServer
+
+	configMtx sync.RWMutex
+	config    *Config
+}
+
+func (p *Plugin) Attest(stream nodeattestorv1.NodeAttestor_AttestServer) error {
+	req, err := stream.Recv()
+	if err != nil {
+		return err
+	}
+
+	c, err := p.getConfig()
+	if err != nil {
+		return err
+	}
+
+	payload := req.GetPayload()
+
+	clientKey := key.NodePublic{}
+	if err := clientKey.UnmarshalText(payload); err != nil {
+		return err
+	}
+
+	tsStatus, err := tailscale.Status(stream.Context())
+	if err != nil {
+		return fmt.Errorf("failed to query local tailscaled status: %w", err)
+	}
+
+	var node *ipnstate.PeerStatus
+
+	if clientKey == tsStatus.Self.PublicKey {
+		node = tsStatus.Self
+	}
+
+	if p, exists := tsStatus.Peer[clientKey]; exists {
+		node = p
+	}
+
+	if node == nil {
+		return fmt.Errorf("unable to find provided client key")
+	}
+
+	id, err := agentID(c.trustDomain, fmt.Sprintf("/%s/%s", PluginName, node.HostName))
+	if err != nil {
+		return err
+	}
+
+	return stream.Send(&nodeattestorv1.AttestResponse{
+		Response: &nodeattestorv1.AttestResponse_AgentAttributes{
+			AgentAttributes: &nodeattestorv1.AgentAttributes{
+				SpiffeId: id.String(),
+			},
+		},
+	})
+}
+
+func (p *Plugin) Configure(ctx context.Context, req *configv1.ConfigureRequest) (*configv1.ConfigureResponse, error) {
+	hclConfig := new(Config)
+	if err := hcl.Decode(hclConfig, req.HclConfiguration); err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "unable to decode configuration: %v", err)
+	}
+
+	if req.CoreConfiguration == nil {
+		return nil, status.Error(codes.InvalidArgument, "global configuration is required")
+	}
+
+	if req.CoreConfiguration.TrustDomain == "" {
+		return nil, status.Error(codes.InvalidArgument, "trust_domain is required")
+	}
+
+	trustDomain, err := spiffeid.TrustDomainFromString(req.CoreConfiguration.TrustDomain)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "trust_domain is invalid: %v", err)
+	}
+
+	hclConfig.trustDomain = trustDomain
+
+	p.setConfig(hclConfig)
+	return &configv1.ConfigureResponse{}, nil
+}
+
+func (p *Plugin) setConfig(config *Config) {
+	p.configMtx.Lock()
+	p.config = config
+	p.configMtx.Unlock()
+}
+
+func (p *Plugin) getConfig() (*Config, error) {
+	p.configMtx.RLock()
+	defer p.configMtx.RUnlock()
+	if p.config == nil {
+		return nil, status.Error(codes.FailedPrecondition, "not configured")
+	}
+	return p.config, nil
+}
+
+func agentID(td spiffeid.TrustDomain, suffix string) (spiffeid.ID, error) {
+	if td.IsZero() {
+		return spiffeid.ID{}, fmt.Errorf("cannot create agent ID with suffix %q for empty trust domain", suffix)
+	}
+	if err := spiffeid.ValidatePath(suffix); err != nil {
+		return spiffeid.ID{}, fmt.Errorf("invalid agent path suffix %q: %w", suffix, err)
+	}
+	return spiffeid.FromPath(td, "/spire/agent"+suffix)
+}
+
+func main() {
+	plugin := new(Plugin)
+	pluginmain.Serve(
+		nodeattestorv1.NodeAttestorPluginServer(plugin),
+		configv1.ConfigServiceServer(plugin),
+	)
+}
---
+++ go.mod
@@ -0,0 +1,43 @@
+module github.com/jsiebens/spire-tailscale-plugin
+
+go 1.18
+
+require (
+	github.com/hashicorp/hcl v1.0.0
+	github.com/spiffe/go-spiffe/v2 v2.0.0
+	github.com/spiffe/spire-plugin-sdk v1.2.2
+	google.golang.org/grpc v1.46.0
+	tailscale.com v1.24.0
+)
+
+require (
+	github.com/akutz/memconn v0.1.0 // indirect
+	github.com/alexbrainman/sspi v0.0.0-20210105120005-909beea2cc74 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.7 // indirect
+	github.com/hashicorp/go-hclog v0.15.0 // indirect
+	github.com/hashicorp/go-plugin v1.4.0 // indirect
+	github.com/hashicorp/yamux v0.0.0-20180604194846-3520598351bb // indirect
+	github.com/josharian/native v1.0.0 // indirect
+	github.com/jsimonetti/rtnetlink v1.1.2-0.20220408201609-d380b505068b // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mdlayher/netlink v1.6.0 // indirect
+	github.com/mdlayher/socket v0.2.3 // indirect
+	github.com/mitchellh/go-ps v1.0.0 // indirect
+	github.com/mitchellh/go-testing-interface v0.0.0-20171004221916-a61a99592b77 // indirect
+	github.com/oklog/run v1.0.0 // indirect
+	go4.org/intern v0.0.0-20211027215823-ae77deb06f29 // indirect
+	go4.org/mem v0.0.0-20210711025021-927187094b94 // indirect
+	go4.org/unsafe/assume-no-moving-gc v0.0.0-20211027215541-db492cf91b37 // indirect
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
+	golang.org/x/net v0.0.0-20220407224826-aac1ed45d8e3 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.zx2c4.com/wireguard/windows v0.4.10 // indirect
+	google.golang.org/genproto v0.0.0-20200806141610-86f49bd18e98 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	inet.af/netaddr v0.0.0-20211027220019-c74959edd3b6 // indirect
+)
