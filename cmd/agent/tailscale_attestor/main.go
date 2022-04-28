c9d31707aae6a6801caa2b6cdfd8e3db02d4ce2d
Johan Siebens <johan.siebens@gmail.com>
2022-04-28 11:19:02 +0200

    feat: use wip tailscale id token

--- cmd/agent/tailscale_attestor/main.go
+++ cmd/agent/tailscale_attestor/main.go
@@ -1,35 +1,13 @@
 package main
 
 import (
+	"github.com/jsiebens/spire-tailscale-plugin/pkg/agent/plugin/nodeattestor/ts"
 	"github.com/spiffe/spire-plugin-sdk/pluginmain"
 	nodeattestorv1 "github.com/spiffe/spire-plugin-sdk/proto/spire/plugin/agent/nodeattestor/v1"
-	"tailscale.com/client/tailscale"
 )
 
-type Plugin struct {
-	nodeattestorv1.UnimplementedNodeAttestorServer
-}
-
-func (p *Plugin) AidAttestation(stream nodeattestorv1.NodeAttestor_AidAttestationServer) error {
-	status, err := tailscale.Status(stream.Context())
-	if err != nil {
-		return err
-	}
-
-	payload, err := status.Self.PublicKey.MarshalText()
-	if err != nil {
-		return err
-	}
-
-	return stream.Send(&nodeattestorv1.PayloadOrChallengeResponse{
-		Data: &nodeattestorv1.PayloadOrChallengeResponse_Payload{
-			Payload: payload,
-		},
-	})
-}
-
 func main() {
-	plugin := new(Plugin)
+	plugin := ts.New()
 	pluginmain.Serve(
 		nodeattestorv1.NodeAttestorPluginServer(plugin),
 	)
